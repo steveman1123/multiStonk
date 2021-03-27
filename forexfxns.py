49d6e700d5428ce4e5cb633ede88dfcc1044e9d4
Steve <smw1109@protonmail.com>
2021-03-26 22:18:26 -0500

    missed adding config files

---
+++ forexfxns.py
@@ -0,0 +1,10 @@
+#functions to interface with the forex.com api
+#http://docs.labs.gaincapital.com/
+#http://faq.labs.gaincapital.com/
+
+
+import otherfxns as o
+
+
+#TODO: figure out how to utilize API
+#add API keys to apikey file
